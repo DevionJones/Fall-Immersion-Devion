c559ab3e1012c6bbae49584f74492ec5de679337
DevionJones <devion.jones@hgs.hiddengeniusproject.org>
2024-11-18 17:48:16 -0800

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,14 @@
-function modelPopUp() {
-    // Your code here.
-}   
+function CloseModal () {
+    //Create a var to store the pop
+    const popup = document.getElementById("myModal");
+
+    //Acces the popup and make it dissappear
+    popup.style.display = "none";
+}
+
+function OpenModal()
+{
+    const popup = document.getElementById("myModal");
+
+    popup.style.display = "block";
+}
