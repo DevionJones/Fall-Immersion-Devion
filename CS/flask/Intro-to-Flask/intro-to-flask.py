8e386755c6a60cff5dc9acc26655be4a4ff1ca63
DevionJones <devion.jones@hgs.hiddengeniusproject.org>
2024-12-04 18:17:03 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,11 @@
-# instruction will below. 
+rom flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello yall"
+
+
+if __name__ == '__main__':
+    app.run(debug = True)# instruction will below. 
